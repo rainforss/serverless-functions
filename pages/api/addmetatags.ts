e1fe7e999feaac86f80d61fbe8c8ebcb14aa5574
rainforss <shengyan@ualberta.ca>
2023-03-09 08:57:31 -0700

    updated response

--- pages/api/addmetatags.ts
+++ pages/api/addmetatags.ts
@@ -2,16 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  arr: Array<string>;
+  result: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const originalArr = req.body.arr;
+  const originalArr: Array<string> = req.body.arr;
   originalArr.splice(1, 0, req.body.metatags);
   originalArr.splice(2, 0, "<style>");
-  const newArr = originalArr.join("");
-  res.status(200).json(newArr);
+  const newHTML = originalArr.join("");
+  res.status(200).json({ result: newHTML });
 }
